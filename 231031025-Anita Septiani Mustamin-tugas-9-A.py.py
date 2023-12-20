7b0f5aec46d4e38c29b4e5850847869d9455be19
anitaseptianiiiiiii <153788051+anitaseptianiiiiiii@users.noreply.github.com>
2023-12-20 23:36:30 +0800

    Add files via upload

---
+++ 231031025-Anita Septiani Mustamin-tugas-9-A.py.py
@@ -0,0 +1,22 @@
+data= {'nama lengkap'        : 'Anita Septiani Mustamin',
+       'umur'                : '18 Tahun',
+       'agama'               : 'Islam',
+       'nama panggilan '     : 'Nita',
+       'jenis kelamin'       : 'Perempuan',
+       'alamat'              : 'Jl. Messang Bau Massepe No.23',
+       'kel'                 : 'Kampung Pisang',
+       'kec'                 : 'Soreang',
+       'kota'                : 'Parepare',
+       'PTN'                 : 'Institut Teknologi B.J. Habibie (ITH)',
+       'program studi'       : 'Sistem Informasi',
+       'angkatan'            : '2023',
+        }
+
+print('Nama                 :', data['nama lengkap'])
+print('Umur                 :', data['umur'])
+print('Jenis kelamin        :', data['jenis kelamin'])
+print('Alamat               :', data['alamat'])
+print('Kelurahan            :', data['kel'])
+print('Perguruan Tinggi     :', data['PTN'])
+print('Program Studi        :', data['program studi'])
+print('Angkatan             :', data['angkatan'])
